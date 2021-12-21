eef4634baff5cf52eeeaf8f8185557d5b8bc6419
zhumgal.akaev.kg <jumgal.akaev@zensoft.io>
2021-12-21 21:07:00 +0600

    added button to put element inside table

--- classes/classes.js
+++ classes/classes.js
@@ -5,22 +5,30 @@ function generateId() {
     return uniqid
 }
 
+// Class - Корзина покупок
+class Basket {
+    constructor(goodName, hasDiscount = false, price = 0) {
+        this.id = generateId();
+        this.goodName = goodName;
+        this.hasDiscount = hasDiscount;
+        this.price = price
+    }
+}
+
 window.addEventListener('DOMContentLoaded', () => {
     const app = document.querySelector('#app');
-
-    // Class - Корзина покупок
-    class Basket {
-        constructor(goodName, hasDiscount = false, price = 0) {
-            this.id = generateId();
-            this.goodName = goodName;
-            this.hasDiscount = hasDiscount;
-            this.price = price
-        }
-    }
     
     // Генерация DOM объектов
     const headerItems = ['ID', 'Название Товара', 'Цена']
     const basketList = [];
+
+    function addGood() {
+        const name = prompt('Введите название');
+        const price = prompt('Введите цену');
+        console.log(Number(price))
+        createGood(new Basket(name, false, Number(price)));
+    }
+
     const basket = new Basket('sneakers', true, 350);
     basketList.push(basket)
     basketList.push(new Basket('Lamp', false, 600))
@@ -41,7 +49,7 @@ window.addEventListener('DOMContentLoaded', () => {
         tr.appendChild(td);
     })
 
-    basketList.forEach((basketItem) => {
+    function createGood(basketItem) {
         const basketContainer = document.createElement('tr')
 
         const td1 = document.createElement('td');
@@ -57,8 +65,17 @@ window.addEventListener('DOMContentLoaded', () => {
         basketContainer.appendChild(td2)
         basketContainer.appendChild(td3)
         tbody.appendChild(basketContainer)
-    })
+    }
+
+    basketList.forEach(createGood)
     table.appendChild(tbody)
 
+
+    const addButton = document.createElement('button');
+    addButton.innerText = 'Добавить';
+    addButton.addEventListener('click', addGood);
+
+    // Вход в программу
+    app.appendChild(addButton);
     app.appendChild(table);
 });
