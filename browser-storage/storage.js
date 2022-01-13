704d39f851aa5564d3179c7cbbac42ed970377cf
zhumgal.akaev.kg <jumgal.akaev@zensoft.io>
2022-01-13 16:17:45 +0600

    file uploader

--- browser-storage/storage.js
+++ browser-storage/storage.js
@@ -1,3 +1,17 @@
+// Загрузка файлов
+const upload = document.querySelector('#upload')
+
+upload.addEventListener('change', (event) => {
+    const blob = new Blob(event.target.files)
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: blob,
+        headers: {
+            'Content-Type': 'application/octet-stream'
+        }
+    })
+})
+
 const STORAGE_KEY = 'accessToken';
 
 const userCredentials = {
@@ -13,7 +27,28 @@ if (!sessionStorage.length) {
     sessionStorage.setItem(STORAGE_KEY, JSON.stringify(userCredentials))
 }
 
-Cookies.set('user', JSON.stringify(userCredentials))
+Cookies.set('user', JSON.stringify(userCredentials), {
+    expires: 7,
+    domain: 'http://127.0.0.1:5500/',
+})
+
+// Основные HTTP методы: GET, POST, PUT, PATCH, DELETE
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+        userId: 1,
+        title: 'Wow new post!',
+        body: 'Description of new post!'
+    }),
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+    },
+})
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'GET',
+})
 
 console.log(Cookies.get('user'))
 console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)))
